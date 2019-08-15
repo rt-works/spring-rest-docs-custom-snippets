cfeeaf9bef34f535e9cf83f66f7d9e0dda7dadfd
Roman Tuchin <roman.tuchin@kreuzwerker.de>
2019-08-15 13:28:45 +0200

    Playing around with coroutines

--- src/main/kotlin/de/xw/contactapi/handler/CreateContactHandler.kt
+++ src/main/kotlin/de/xw/contactapi/handler/CreateContactHandler.kt
@@ -3,6 +3,9 @@ package de.xw.contactapi.handler
 import io.ktor.application.ApplicationCall
 import io.ktor.request.receive
 import io.ktor.response.respond
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import mu.KLogging
 import java.util.*
 
@@ -10,8 +13,13 @@ class CreateContactHandler {
     companion object : KLogging()
 
     suspend fun handle(call: ApplicationCall) {
-        val request = call.receive(CreateContactRequest::class)
-        logger.debug { "handling create contact request: $request" }
+        GlobalScope.launch {
+            // launch a new coroutine in background and continue
+            val request = call.receive(CreateContactRequest::class)
+            logger.debug { "handling create contact request: $request" }
+            delay(2000)
+        }
+
         //do handling
         val response = CreateContactResponse(UUID.randomUUID().toString())
         call.respond(response)
