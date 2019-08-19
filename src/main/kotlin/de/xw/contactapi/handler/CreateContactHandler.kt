a50baff1256a82fb50176679fec0e2bc382788a7
Roman Tuchin <roman.tuchin@kreuzwerker.de>
2019-08-19 11:09:49 +0200

    play around with coroutines

--- src/main/kotlin/de/xw/contactapi/handler/CreateContactHandler.kt
+++ src/main/kotlin/de/xw/contactapi/handler/CreateContactHandler.kt
@@ -4,25 +4,51 @@ import io.ktor.application.ApplicationCall
 import io.ktor.request.receive
 import io.ktor.response.respond
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import mu.KLogging
 import java.util.*
+import kotlin.system.measureTimeMillis
 
 class CreateContactHandler {
     companion object : KLogging()
 
     suspend fun handle(call: ApplicationCall) {
-        GlobalScope.launch {
-            // launch a new coroutine in background and continue
-            val request = call.receive(CreateContactRequest::class)
-            logger.debug { "handling create contact request: $request" }
-            delay(2000)
+
+        val time = measureTimeMillis {
+            coroutineScope {
+                val requestDeffered = GlobalScope.async {
+                    logger.debug { "Getting request" }
+                    delay(2000L)
+                    call.receive(CreateContactRequest::class)
+                }
+
+
+                val smthDeferred = GlobalScope.async {
+                    doSomethingUseful()
+                }
+
+                val request = requestDeffered.await()
+                val smth = smthDeferred.await()
+
+                logger.debug(fun(): Any? {
+                    return "handling create contact request: ${request} + $smth"
+                })
+            }
         }
+
+
+        logger.info { "Completed in $time ms" }
 
         //do handling
         val response = CreateContactResponse(UUID.randomUUID().toString())
         call.respond(response)
+    }
+
+    private suspend fun doSomethingUseful() {
+        delay(2000L)
+        logger.debug { "doin' smt useful" }
     }
 }
 
